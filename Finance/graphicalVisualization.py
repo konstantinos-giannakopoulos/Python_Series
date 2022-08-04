93feca6eae67931b9e3f7f4c6580bd25afa8983f
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-04 20:41:30 +0800

    Update graphs with candlestick

--- Finance/graphicalVisualization.py
+++ Finance/graphicalVisualization.py
@@ -50,3 +50,37 @@ plt.title('AMZN Share Price')
 
 plt.tight_layout()
 plt.show()
+
+# 2) Candlestick Charts
+from mpl_finance import candlestick_ohlc
+import matplotlib.dates as mdates
+
+start = dt.datetime(2017,1,1)
+end = dt.datetime(2022,7,31)
+
+apple = apple[['Open', 'High', 'Low', 'Close']]
+
+apple.reset_index(inplace=True)
+apple['Date']=apple['Date'].map(mdates.date2num)
+
+ax = plt.subplot()
+candlestick_ohlc(ax, apple.values, width=5, colordown='r', colorup='g')
+ax.grid()
+ax.xaxis_date()
+plt.show()
+
+# Plotting multiple days
+apple = web.DataReader('AAPL', 'yahoo', start, end)
+apple_ohlc = apple['Adj Close'].resample('10D').ohlc()
+apple_ohlc.reset_index(inplace=True)
+apple_ohlc['Date'] = apple_ohlc['Date'].map(mdates.date2num)
+apple_volume = apple['Volume'].resample('10D').sum()
+
+ax1 = plt.subplot2grid((6,1),(0,0),rowspan=4, colspan=1)
+ax2 = plt.subplot2grid((6,1),(4,0),rowspan=4, colspan=1, sharex=ax1)
+ax1.xaxis_date()
+
+candlestick_ohlc(ax1, apple_ohlc.values, width=5, colorup='g', colordown='r')
+ax2.fill_between(apple_volume.index.map(mdates.date2num),apple_volume.values)
+plt.tight_layout()
+plt.show()
